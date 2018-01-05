4fa6968a93b6124948f7c94dda69b53d2fb5161b
Angelicavc21 <angelik_m21@hotmail.com>
2018-01-05 12:06:59 -0500

    practicando farebase

--- js/app.js
+++ js/app.js
@@ -1,32 +1,30 @@
-;(function(){
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCNcFyocem97d1fF_ftgMG8j-XQWgUIjQY",
-  authDomain: "laboratoria-test-a71b4.firebaseapp.com",
-  databaseURL: "https://laboratoria-test-a71b4.firebaseio.com",
-  projectId: "laboratoria-test-a71b4",
-  storageBucket: "laboratoria-test-a71b4.appspot.com",
-  messagingSenderId: "1002692745055"
-};
-firebase.initializeApp(config);
+; (function () {
+  var config = {
+    apiKey: "AIzaSyCNcFyocem97d1fF_ftgMG8j-XQWgUIjQY",
+    authDomain: "laboratoria-test-a71b4.firebaseapp.com",
+    databaseURL: "https://laboratoria-test-a71b4.firebaseio.com",
+    projectId: "laboratoria-test-a71b4",
+    storageBucket: "laboratoria-test-a71b4.appspot.com",
+    messagingSenderId: "1002692745055"
+  };
 
+  firebase.initializeApp(config);
 
-$btn = $('#butto');
+  $btn = $('#butto');
 
-$user = null;
+  $user = null;
 
-function signInGoogle() {
-  console.log('hola')
-  var provider = new firebase.auth.GoogleAuthProvider();
+  function signInGoogle() {
+    console.log('hola')
+    var provider = new firebase.auth.GoogleAuthProvider();
 
-  firebase.auth().signInWithPopup(provider).then(function (result) {
-    var user = result.user;
-    // ...
-    console.log($user);
-  })
-}
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+      $user = result.user;
 
+      console.log($user);
+    })
+  }
 
-$btn.on('click', signInGoogle);
+  $btn.on('click', signInGoogle);
 
 })();
